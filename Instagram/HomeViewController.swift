a8a6bb93a19bb5a285b0ec53e610eb215daca2a7
yasainojikan <imtmtmhr2@gmail.com>
2019-03-22 22:57:36 +0900

    Podfile,Podfile.lock, . gitignoreを作成

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by 今冨友裕 on 2019/03/22.
+//  Copyright © 2019年 yasainojikan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 今冨友裕 on 2019/03/22.
+//  Copyright © 2019年 yasainojikan. All rights reserved.
+//
+
+import Foundation
