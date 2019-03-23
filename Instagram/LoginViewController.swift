c0cbbd5547ad731159f810db72682f025b01b4b9
yasainojikan <imtmtmhr2@gmail.com>
2019-03-24 00:13:55 +0900

    ログインボタンのActionと、SVProgressHUDを設定

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
     
@@ -19,6 +20,32 @@ class LoginViewController: UIViewController {
     // ログインボタンをタップした時に呼ばれるメソッド
     // if letを使えば、addressをunwrapせずともインスタンスとして使える
     @IBAction func handleLoginButton(_ sender: Any) {
+        if let address = mailAddressTextField.text,
+            let password = passwordTextField.text {
+            //  アドレスとパスワードいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty{
+                SVProgressHUD.showError(withStatus: "必要項目を入力してください")
+                return
+            }
+            
+            // HUDで処理中を表示
+            SVProgressHUD.show()
+            
+            Auth.auth().signIn(withEmail: address, password: password) { user, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました")
+                    return
+                }
+                print("DEBUG_PRINT: ログインに成功しました。")
+                
+                // HUDを消す
+                SVProgressHUD.dismiss()
+                
+                // 画面を閉じてViewControllerに戻る
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
         
     }
     
@@ -31,8 +58,12 @@ class LoginViewController: UIViewController {
             // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
             if address.isEmpty || password.isEmpty || displayName.isEmpty {
                 print("DEBUG_PRINT: 何かが空文字です。")
+                SVProgressHUD.showError(withStatus: "必要項目を入力してください")
                 return
             }
+            
+            // HUDで処理中を表示
+            SVProgressHUD.show()
             
             // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする。
             // if letのerrorとerror in は違うerror
@@ -40,11 +71,12 @@ class LoginViewController: UIViewController {
                 if let error = error {
                     //  エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "ユーザー作成に失敗しました")
                     return
                 }
                 print("DEBUG_PRINT: ユーザー作成に成功しました。")
                 
-                //  表示名を設定する。userは2種類の定義に注意
+                //  表示名を設定する。userは2種類あることに注意
                 let user = Auth.auth().currentUser
                 if let user = user {
                     let changeRequest = user.createProfileChangeRequest()
@@ -53,9 +85,13 @@ class LoginViewController: UIViewController {
                         if let error = error {
                             //  プロフィールの更新でエラーが発生
                             print("DEBUG_PRINT: " + error.localizedDescription)
+                            SVProgressHUD.showError(withStatus: "表示名の設定に失敗しました。")
                             return
                         }
                         print("DEBUG_PRINT: [displayName = \(user.displayName!)の設定に成功しました。")
+                        
+                        // HUDを消す
+                        SVProgressHUD.dismiss()
                         
                         // 画面を閉じてViewControllerに戻る
                         self.dismiss(animated: true, completion: nil)
--- Podfile
+++ Podfile
@@ -11,5 +11,6 @@ pod 'Firebase','5.16.0'
 pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Database'
+pod 'SVProgressHUD','2.2.5'
 
 end
